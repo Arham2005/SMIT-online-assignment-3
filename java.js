f4da0ace9dbdad410b8de79f6590068be5d8d346
Arham2005 <125215131+Arham2005@users.noreply.github.com>
2023-04-06 20:20:43 +0500

    files uploaded

---
+++ java.js
@@ -0,0 +1,17 @@
+let num1 = +prompt("Enter Any Number");
+let num2 = +prompt("Enter Any Number");
+let num3 = +prompt("Enter Any Number");
+let num4 = +prompt("Enter Any Number");
+let num5 = +prompt("Enter Any Number");
+
+if (num1 > num2) {
+    console.log(num1);
+}else if (num2 > num3) {
+    console.log(num2);
+}else if (num3 > num4) {
+    console.log(num3);
+}else if (num4 > num5) {
+    console.log(num4);
+}else {
+    console.log(num5);
+}
